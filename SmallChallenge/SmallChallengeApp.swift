7c6c87b3ab42636516b1d8ef3c986b5b4435420a
juh2 <juh2.dev@gmail.com>
2025-11-27 14:08:35 +0900

    Initial Commit

---
+++ SmallChallenge/SmallChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmallChallengeApp.swift
+//  SmallChallenge
+//
+//  Created by 임주희 on 11/27/25.
+//
+
+import SwiftUI
+
+@main
+struct SmallChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SmallChallengeTests/SmallChallengeTests.swift
@@ -0,0 +1,17 @@
+//
+//  SmallChallengeTests.swift
+//  SmallChallengeTests
+//
+//  Created by 임주희 on 11/27/25.
+//
+
+import Testing
+@testable import SmallChallenge
+
+struct SmallChallengeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
