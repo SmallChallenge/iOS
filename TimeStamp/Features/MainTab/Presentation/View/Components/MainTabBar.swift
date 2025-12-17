43c26a84d5c18c71c82b6e9d51cc135c90673cf8
juh2 <juh2.dev@gmail.com>
2025-12-17 21:47:35 +0900

    fix: 커스텀 탭뷰 UI수정

--- TimeStamp/Features/MainTab/Presentation/View/Components/MainTabBar.swift
+++ TimeStamp/Features/MainTab/Presentation/View/Components/MainTabBar.swift
@@ -13,38 +13,47 @@ struct MainTabBar: View {
     @Binding var showCamera: Bool
 
     var body: some View {
-        HStack(spacing: 0) {
-            
-            // MARK: 내 기록
-            TabButton(
-                type: .myLog,
-                isSelected: selectedTab == 0) {
-                selectedTab = 0
-            }
-            
-            // MARK: 카메라
-            Button {
-                showCamera = true
-            } label: {
-                AddButtonView()
-            }
-            .offset(y: -20) // 위로 튀어나오게
-            
-            
-            // MARK: 커뮤니티
-            TabButton(
-                type: .community,
-                isSelected: selectedTab == 2) {
-                    selectedTab = 2
+        GeometryReader { geometry in
+            VStack(spacing: 0) {
+                HStack(spacing: 0) {
+
+                    // MARK: 내 기록
+                    TabButton(
+                        type: .myLog,
+                        isSelected: selectedTab == 0) {
+                        selectedTab = 0
+                    }
+
+                    // MARK: 카메라
+                    Button {
+                        showCamera = true
+                    } label: {
+                        AddButtonView()
+                    }
+                    .offset(y: -20) // 위로 튀어나오게
+
+
+                    // MARK: 커뮤니티
+                    TabButton(
+                        type: .community,
+                        isSelected: selectedTab == 2) {
+                            selectedTab = 2
+                        }
+
+
                 }
-            
-            
+                .frame(height: 58)
+
+                // Safe Area 영역 채우기
+                Color.gray900
+                    .frame(height: geometry.safeAreaInsets.bottom)
+            }
+            .background(
+                Color.gray900
+            )
         }
         .frame(height: 58)
-        .background(
-            Color.gray900
-                .shadow(color: .black.opacity(0.1), radius: 5, y: -2)
-        )
+        .ignoresSafeArea(edges: .bottom)
     }
 }
 
