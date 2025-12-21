132d95ded0bf55039c83af7ad345cf4dcaf85a16
juh2 <juh2.dev@gmail.com>
2025-12-21 20:11:23 +0900

    chore: kakao sdk(spm) 설치, 기본 설정

--- TimeStamp/Application/TimeStampApp.swift
+++ TimeStamp/Application/TimeStampApp.swift
@@ -6,6 +6,8 @@
 //
 
 import SwiftUI
+import KakaoSDKCommon
+import KakaoSDKAuth
 
 // 화면 회전 제어를 위한 AppDelegate
 class AppDelegate: NSObject, UIApplicationDelegate {
@@ -20,9 +22,21 @@ class AppDelegate: NSObject, UIApplicationDelegate {
 struct TimeStampApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     
+    init() {
+        // Kakao SDK 초기화
+        KakaoSDK.initSDK(appKey: "971afea14146fb0c32c67bff9840350e")
+    }
+    
     var body: some Scene {
         WindowGroup {
             MainTabView()
+            // onOpenURL()을 사용해 커스텀 URL 스킴 처리 (kakao)
+                .onOpenURL(perform: { url in
+                    if (AuthApi.isKakaoTalkLoginUrl(url)) {
+                        AuthController.handleOpenUrl(url: url)
+                    }
+                })
         }
     }
+    
 }
