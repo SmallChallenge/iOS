b1b8bf0d6c2349e4051985ef7f77be43c2426a86
juh2 <juh2.dev@gmail.com>
2026-01-03 19:45:47 +0900

    fix: mylog emptyView  수정

--- TimeStamp/Features/MyLog/MyLog/Presentation/View/Components/MyLogEmptyView.swift
+++ TimeStamp/Features/MyLog/MyLog/Presentation/View/Components/MyLogEmptyView.swift
@@ -17,27 +17,19 @@ struct MyLogEmptyView: View {
                 .frame(width: 140, height: 140)
             
 
-            VStack(alignment: .leading, spacing: 4) {
+            VStack(alignment: .center, spacing: 4) {
                 Text("아직 남긴 기록이 없어요.")
                     .font(.Body1)
                     .foregroundStyle(Color.gray500)
                 
-                HStack(spacing: 0) {
+                
                     Text("+ 버튼")
-                        .font(.Body1)
-                        .overlay(
-                            Rectangle()
-                                .fill(Color.gray500)
-                                .frame(height: 1)
-                                .offset(y: 2),
-                            alignment: .bottom
-                        )
-                        .foregroundStyle(Color.gray500)
-                    Text("을 눌러 기록을 시작해보세요!")
-                        .font(.Body1)
-                        .foregroundStyle(Color.gray500)
-                }
+                    .font(FontStyle.SubTitle1.font)
+                    + Text("을 눌러 기록을 시작해보세요!")
+                    .font(FontStyle.Body1.font)
+                        
             }
+            .foregroundStyle(Color.gray500)
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
